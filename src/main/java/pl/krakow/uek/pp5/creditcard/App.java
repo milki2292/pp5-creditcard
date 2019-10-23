6878e765d8e958b9fbe514391bcc0b13cd877dbb
milki2292 <kamil.szmuc2292@gmail.com>
2019-10-23 10:51:05 +0200

    wewrfwf
    src/main/java/pl/krakow/uek/pp5/creditcard/App.java

--- src/main/java/pl/krakow/uek/pp5/creditcard/App.java
+++ src/main/java/pl/krakow/uek/pp5/creditcard/App.java
@@ -2,6 +2,6 @@ package pl.krakow.uek.pp5.creditcard;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Elo mordo");
+        System.out.println("Elo mordo :)");
     }
 }
